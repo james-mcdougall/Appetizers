e169366f67b5c8cb44ca4c6df8fa810513edf2e4
James McDougall <jamesmcdougall@Jamess-MacBook-Pro.local>
2021-09-04 15:58:50 -0500

    Safety push

--- Appetizers/Appetizers/AppetizerTabView.swift
+++ Appetizers/Appetizers/AppetizerTabView.swift
@@ -8,32 +8,33 @@
 import SwiftUI
 
 struct AppetizerTabView: View {
-	var body: some View {
-		TabView {
-			AppetizerListView()
-				.tabItem {
-					Image(systemName: "house")
-					Text("Home")
-				}
-			
-			AccountView()
-				.tabItem {
-					Image(systemName: "person")
-					Text("Account")
-				}
-			
-			OrderView()
-				.tabItem {
-					Image(systemName: "bag")
-					Text("Order")
-				}
-			}
-}
-	
-	
-	struct ContentView_Previews: PreviewProvider {
-		static var previews: some View {
-			AppetizerTabView()
-		}
-	}
+    var body: some View {
+        TabView {
+            AppetizerListView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            
+            AccountView()
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Account")
+                }
+            
+            OrderView()
+                .tabItem {
+                    Image(systemName: "bag")
+                    Text("Order")
+                }
+        }
+        .accentColor(Color("brandPrimary"))
+    }
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            AppetizerTabView()
+        }
+    }
 }
