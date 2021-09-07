93bf8be9704ab4ea2efa63c9a3970a6c97b6d59b
james-mcdougall <jameswilliammcdougall@gmail.com>
2021-09-06 19:57:10 -0500

    Created activity indicator for loading screens

---
+++ Appetizers/Appetizers/Color+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Ext.swift
+//  Appetizers
+//
+//  Created by James McDougall on 9/6/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary = Color("brandPrimary")
+}
+
+extension UIColor {
+    static let brandPrimary = UIColor(named: "brandPrimary")
+}
